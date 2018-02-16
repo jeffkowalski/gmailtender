686ec62a584340c7a8bfc5ca83f8fb248a63dcba
Jeff Kowalski <jeff.kowalski@gmail.com>
2018-02-15 18:45:46 -0800

    Adjust amex statement match

--- gmailtender.rb
+++ gmailtender.rb
@@ -304,6 +304,49 @@ class MH_SquareReceipt < MessageHandler
 end
 
 
+class MH_CloverReceipt < MessageHandler
+  def self.match headers
+    headers['Subject']&.include?("Your receipt from") &&
+      headers['From']&.include?("<app@clover.com>")
+  end
+
+  def handle message, headers
+    payload = (gmail.get_user_message 'me', message.id).payload
+    body = payload.parts[0].body.data;
+
+    # $6.95 total
+
+    amount = '' + body[/(\$\d+\.\d+) total/, 1]
+    payee = '' +  headers['Subject'][/Your receipt from (.*)/, 1].downcase
+    return make_org_entry "receipt for #{payee}", "@quicken", '#C',
+                          "<#{Time.now.strftime('%F %a')}>",
+                          amount + "\n" + "https://mail.google.com/mail/u/0/#inbox/#{message.id}"
+  end
+end
+
+
+class MH_SquareReceipt < MessageHandler
+  def self.match headers
+    headers['Subject']&.include?("Receipt from") &&
+      headers['From']&.include?("via Square <receipts@messaging.squareup.com>")
+  end
+
+  def handle message, headers
+    payload = (gmail.get_user_message 'me', message.id).payload
+    body = payload.parts[0].body.data;
+
+    # You paid $10.00 with your AMEX ending in 2008 to Pizza Politana on Aug 22
+
+    amount = '' + body[/You paid (\$\d+\.\d+) with/, 1]
+    account = '' + body[/with your (\S+) ending in/, 1].downcase
+    payee = '' + body[/to (.*?) on/, 1].downcase
+    return make_org_entry "receipt for #{payee}", "#{account}:@quicken", '#C',
+                          "<#{Time.now.strftime('%F %a')}>",
+                          amount + "\n" + "https://mail.google.com/mail/u/0/#inbox/#{message.id}"
+  end
+end
+
+
 class MH_PershingStatement < MessageHandler
   def self.match headers
     headers['Subject'] =='Account Statement Notification' &&
@@ -391,7 +434,7 @@ end
 
 class MH_AmericanExpressStatement < MessageHandler
   def self.match headers
-    headers['Subject']&.index(/Statement is Ready/) &&
+    headers['Subject']&.index(/Important Notice: Your .* Statement/) &&
       headers['From'] == 'American Express <AmericanExpress@welcome.aexp.com>'
   end
 
